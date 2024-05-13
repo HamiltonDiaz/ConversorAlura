a1cb65c97afcfbc179ce240e67dfdbfb9f96aaf8
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2024-05-13 10:27:00 -0500

    Creación proyecto para desafio alura

---
+++ src/Principal.java
@@ -0,0 +1,23 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        String menu = """
+        ***********************************************
+        Sea bienvenido/a al conversor de moneda =
+        
+        1) Dólar =>> Peso argentino
+        2) Peso argentino =>> Dólar
+        3) Dólar =>> Real brasileño
+        4) Real brasileño =>> Dólar
+        5) Dólar =>> Peso colombiano
+        6) Peso colombiano =>> Dólar
+        7) Salir
+        ***********************************************
+        """;
+
+        System.out.println(menu);
+
+
+
+    }
+}
